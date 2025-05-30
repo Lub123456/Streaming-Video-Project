bce46d37fe910ac41c801b0feb9b47edb337f12c
Lub123456 <93678601+Lub123456@users.noreply.github.com>
2025-05-30 18:53:30 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import fr.bmartel.speedtest.SpeedTestSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! Hello, World!");
     }
+    SpeedTestSocket socket;
 }

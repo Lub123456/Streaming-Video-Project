7cb4ca9a99a05edad56f07becdbc9acba2c1ed23
Lub123456 <93678601+Lub123456@users.noreply.github.com>
2025-06-13 16:32:00 +0300

    Update FfmpegCommandRunner.java

--- src/main/java/utils/FfmpegCommandRunner.java
+++ src/main/java/utils/FfmpegCommandRunner.java
@@ -7,19 +7,19 @@ public class FfmpegCommandRunner {
     // Converts a video file to a specified resolution using ffmpeg
     public static void convert(String inputPath, String outputPath, String resolution) {
         String scale = switch (resolution) {
-            case "240p" -> "426x240";
-            case "360p" -> "640x360";
-            case "480p" -> "854x480";
-            case "720p" -> "1280x720";
-            case "1080p" -> "1920x1080";
+            case "240p" -> "-2:240";
+            case "360p" -> "-2:360";
+            case "480p" -> "-2:480";
+            case "720p" -> "-2:720";
+            case "1080p" -> "-2:1080";
             default -> "";
         };
 
         ProcessBuilder builder = new ProcessBuilder(
-                "ffmpeg", "-y", "-i", inputPath, "-s", scale, outputPath
+                "ffmpeg", "-y", "-i", inputPath, "-c:v", "libx264", "-crf", "25", "-vf", "scale="+scale, outputPath
         );
 
-        builder.inheritIO(); // pour voir les logs ffmpeg
+        builder.inheritIO();
         try {
             Process process = builder.start();
             process.waitFor();
